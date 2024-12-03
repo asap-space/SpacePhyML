f5352d36cd3c202dbf9d3ab79102a7276788fca1
Jonah Ekelund <73965092+Jonah-E@users.noreply.github.com>
2024-12-03 13:40:59 +0100

    Add setup and __init__

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='SpacePhyML',
+    version='0.1.0',
+    description='A Framework for distributin Machine Learning Datasets for Space and Plasma physics.',
+    url='https://github.com/Jonah-E/SpacePhyML',
+    author='Jonah Ekelund',
+    author_email='jonahek@kth.se',
+    license='MIT License',
+    packages=['spacephyml'],
+    install_requires=['torch',
+                      ],
+
+    classifiers=[
+    ],
+)
+
---
+++ spacephyml/__init__.py
@@ -0,0 +1,10 @@
+"""
+SpachePhyML 
+
+
+"""
+
+__version__ = '0.1.0'
+__author__  = 'Jonah Ekelund'
+__credits__ = ''
+
