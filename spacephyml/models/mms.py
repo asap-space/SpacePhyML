5b563a30f9a3c10f93e60508ddd40181d0ebd1ac
Jonah Ekelund <73965092+Jonah-E@users.noreply.github.com>
2025-04-04 11:37:03 +0200

    Add PCBasline model

--- spacephyml/models/mms.py
+++ spacephyml/models/mms.py
@@ -1,5 +1,53 @@
-from .arcs.mms import PCReduced_arc
+from os import makedirs
 
+from torch import load as torch_load
+
+from .arcs.mms import PCReduced_arc, PCBaseline_arc
+from ..utils.file_download import missing_files, download_file_with_status
+
+class PCBaseline(PCBaseline_arc):
+    """
+    Load a train PCBaseline model for MMS dayside plasma region clessification.
+
+    Examples:
+        >>> from spacephyml.models.mms import PCBaseline
+        >>> model = PCBaseline('s84')
+
+    Args:
+        model (string): The model to load, ['s42', 's84', 's168', 's336']
+        path (string): Path to loacation of stored model.
+    """
+
+    _models = {'s42': {
+                    'url': '',
+                    'file': 'model_PCBaseline_s42.pth'},
+               's84': {
+                    'url': '',
+                    'file': 'model_PCBaseline_s84.pth'},
+               's168': {
+                    'url': '',
+                    'file': 'model_PCBaseline_s168.pth'},
+               's336': {
+                    'url': '',
+                    'file': 'model_PCBaseline_s336.pth'},
+               }
+
+    def __init__(self, model='s42', path = './models'):
+        super().__init__()
+
+        if model not in self._models.keys():
+            raise ValueError(f'Incorrect model, {model} not in' +
+                             f'{self._models.keys()}')
+
+        filepath = f'{path}/' + self._models[model]['file']
+
+        missing = missing_files([filepath], './')
+        if missing:
+            print('Missing model file, downloading')
+            makedirs(path, exist_ok=True)
+            download_file_with_status(self._models[model]['url'], filepath)
+
+        self.classifier.load_state_dict(torch_load(filepath, weights_only=True))
 
 class PCReduced(PCReduced_arc):
     """
@@ -28,7 +76,7 @@ class PCReduced(PCReduced_arc):
                     'file': 'model_PCReduced_s336.pth'},
                }
 
-    def __init__(self, model='42', path = './model'):
+    def __init__(self, model='s42', path = './models'):
         super().__init__()
 
         if model not in self._models.keys():
@@ -39,8 +87,8 @@ class PCReduced(PCReduced_arc):
 
         missing = missing_files([filepath], './')
         if missing:
-            print('Missing dataset file, downloading')
+            print('Missing model file, downloading')
             makedirs(path, exist_ok=True)
             download_file_with_status(self._models[model]['url'], filepath)
 
-        self.classifier.load_state_dict(torch.load(filepath, weights_only=True))
+        self.classifier.load_state_dict(torch_load(filepath, weights_only=True))
